bb473648d2ba01946afa9aad653288210f825ca1
Yeerh <diegoosilvaewerton@gmail.com>
2026-03-02 11:26:58 -0300

    Remove fluxo legado nao utilizado

--- src/lib/flow.storage.ts
+++
@@ -1,9 +0,0 @@
-const Q_KEY = "portal_questionario_completo_v1";
-
-export function setQuestionarioCompleto(v: boolean) {
-  localStorage.setItem(Q_KEY, v ? "1" : "0");
-}
-
-export function isQuestionarioCompleto() {
-  return localStorage.getItem(Q_KEY) === "1";
-}
